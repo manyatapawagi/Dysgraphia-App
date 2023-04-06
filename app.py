83e01b2a88ccdf85d23183280aa9be61c3425c55
manyatapawagi <manyatapawagi@gmail.com>
2023-04-06 19:45:18 +0530

    second commit

--- app.py
+++ app.py
@@ -1,46 +1,70 @@
 from flask import Flask
 from google.cloud import vision
-from flask import Flask, request
-from flask.json import jsonify
+import math
+from protobuf_to_dict import protobuf_to_dict, dict_to_protobuf
+import json
+from google.protobuf import json_format
 
 app = Flask(__name__)
 
 @app.route('/')
 def detect_document_uri():
-        client = vision.ImageAnnotatorClient()
-        image = vision.Image()
-        image.source.image_uri = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
+    client = vision.ImageAnnotatorClient()
+    image = vision.Image()
+    image.source.image_uri = "https://i.imgur.com/hVXJKTW.png"
 
-        response = client.document_text_detection(image=image)
+    response = client.document_text_detection(image=image) 
+    json_string = json_format.MessageToJson(response._pb)
 
-        for page in response.full_text_annotation.pages:
-            for block in page.blocks:
-                print('\nBlock confidence: {}\n'.format(block.confidence))
+    #json_string = open("dummy.txt").read().replace("'",  "\"")
+    
+    responseObj = json.loads(json_string)
+    for page in response.full_text_annotation.pages:
+        block_confidences = []
+        for block in page.blocks:
+            block_confidences.append(block.confidence)
 
-                for paragraph in block.paragraphs:
-                    print('Paragraph confidence: {}'.format(
-                        paragraph.confidence))
+            paragraph_confidences = []
+            for paragraph in block.paragraphs:
+                paragraph_confidences.append(paragraph.confidence)
 
-                    for word in paragraph.words:
-                        word_text = ''.join([
-                            symbol.text for symbol in word.symbols
-                        ])
-                        print('Word text: {} (confidence: {})'.format(
-                            word_text, word.confidence))
+                for word in paragraph.words: 
+                    for symbol in word.symbols:
+                        letterX = [] 
+                        letterY = []
+                        for coord in symbol.bounding_box.vertices:
+                            letterX.append(coord.x) 
+                            letterY.append(coord.y)                       
+                        Lx1 = letterX[0]
+                        Lx2 = letterX[1]
+                        Ly1 = letterY[0]
+                        Ly3 = letterY[2]
+                        width = Lx2 - Lx1
+                        height = Ly3 - Ly1                
+                        print("<p>\n", symbol.text, ": \n width: ", width, "\n height: ", height, "</p>")
+            
+        print('<p>\nParagraph confidence: {}%</p>'.format((math.floor(sum(paragraph_confidences) * 1000)/10)/len(paragraph_confidences)))
+        print('<p>Block confidence: {}%</p>'.format((math.floor(sum(block_confidences) * 1000)/10)/len(block_confidences)))
 
-                        for symbol in word.symbols:
-                            print('\tSymbol: {} (confidence: {})'.format(
-                                symbol.text, symbol.confidence))
+    for listCounter in range(1, len(response.text_annotations)):
+        wordBounding = []
+        wordData = response.text_annotations[listCounter]
 
-        # if response.error.message:
-        #     raise Exception(
-        #         '{}\nFor more info on error messages, check: '
-        #         'https://cloud.google.com/apis/design/errors'.format(
-        #             response.error.message))
+        for boundingCoord in wordData.bounding_poly.vertices:
+            wordBounding.append(boundingCoord.x)
+        print("<p>\n {}: {}</p>".format(wordData.description, wordBounding))
 
-        return jsonify(response)
+    word_gap_list = []
+    for i in range(0, len(response.text_annotations)-1):
+        if (i > 0):
+            W1x = responseObj['textAnnotations'][i]['boundingPoly']['vertices'][1]['x']
+            W2x = responseObj['textAnnotations'][i + 1]['boundingPoly']['vertices'][0]['x']
+            word_gap = W2x - W1x
+            word_gap_list.append(word_gap)
+    print("<p>\n word gap: \n{}</p>".format(word_gap_list))
 
-
+detect_document_uri()
+  
 if __name__ == '__main__':
-        app.debug = True
-        app.run()
+    app.debug = True
+    app.run()
