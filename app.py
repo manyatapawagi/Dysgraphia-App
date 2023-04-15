d2b7f6cc03ef7460f0fd6311037b6471fcd32b59
manyatapawagi <manyatapawagi@gmail.com>
2023-04-15 15:39:52 +0530

    fourteenth commit

--- app.py
+++ app.py
@@ -11,81 +11,83 @@ CORS(app)
 
 @app.route('/uploaded', methods=['POST', 'GET'])
 def receiveData():
-    URL = request.get_json()
-    output = json.loads(URL)
+    try:
+        URL = dict(request.get_json())["url"]
+        client = vision.ImageAnnotatorClient()
+        image = vision.Image()
+        image.source.image_uri = URL
 
-    return URL
+        response = client.document_text_detection(image=image) 
+        json_string = json_format.MessageToJson(response._pb)    
+        responseObj = json.loads(json_string)
+        
+        for page in response.full_text_annotation.pages:
+            block_confidences = []
+            LsizeCont = []
+            dataJson = {}
+            for block in page.blocks:
+                block_confidences.append(block.confidence)
+                paragraph_confidences = []
+                for paragraph in block.paragraphs:
+                    paragraph_confidences.append(paragraph.confidence)
+                    for word in paragraph.words:
+                        
+                        for symbol in word.symbols:
+                            letterX = [] 
+                            letterY = []
+                            for coord in symbol.bounding_box.vertices:
+                                letterX.append(coord.x) 
+                                letterY.append(coord.y)                       
+                            Lx1 = letterX[0]
+                            Lx2 = letterX[1]
+                            Ly1 = letterY[0]
+                            Ly3 = letterY[2]
+                            width = Lx2 - Lx1
+                            height = Ly3 - Ly1  
+                            Lsize = {symbol.text: {"width": width, "height": height}}
+                            
+                            LsizeCont.append(Lsize)
+                            dataJson["Letter-Sizes"] = LsizeCont
+            Pconf = math.floor(sum(paragraph_confidences) * 1000)/10/len(paragraph_confidences)
+            Bconf = math.floor(sum(block_confidences) * 1000)/10/len(block_confidences)
+            dataJson["Paragraph-Confidence"] = Pconf
+            dataJson["Block-Confidence"] = Bconf
+        WsizeCont = []
+        for listCounter in range(1, len(response.text_annotations)):
+            wordBounding = []
+            
+            wordData = response.text_annotations[listCounter]
+
+            for boundingCoord in wordData.bounding_poly.vertices:
+                wordBounding.append(boundingCoord.x)
+            Wsize = {wordData.description: wordBounding}
+
+            WsizeCont.append(Wsize)
+            dataJson["Word-Bounding"] = WsizeCont
+
+        word_gap_list = []
+        for i in range(0, len(response.text_annotations)-1):
+            if (i > 0):
+                W1x = responseObj['textAnnotations'][i]['boundingPoly']['vertices'][1]['x']
+                W2x = responseObj['textAnnotations'][i + 1]['boundingPoly']['vertices'][0]['x']
+                word_gap = W2x - W1x
+                word_gap_list.append(word_gap)
+        dataJson["Gaps"] = word_gap_list
+        dataJson["Full-text"] = response.text_annotations[0].description
+
+        return dataJson
+    except:
+        return "{}"
 
 # def detect_document_uri():
-    
-#     data = {}
+
 #     try:
 #         data = dict(request.get_json())
 #     except:
 #         pass
 #     finally:                
 #         with app.app_context():              
-#             client = vision.ImageAnnotatorClient()
-#             image = vision.Image()
-#             image.source.image_uri = URL
-
-#             response = client.document_text_detection(image=image) 
-#             json_string = json_format.MessageToJson(response._pb)    
-#             responseObj = json.loads(json_string)
             
-#             for page in response.full_text_annotation.pages:
-#                 block_confidences = []
-#                 LsizeCont = []
-#                 dataJson = {}
-#                 for block in page.blocks:
-#                     block_confidences.append(block.confidence)
-#                     paragraph_confidences = []
-#                     for paragraph in block.paragraphs:
-#                         paragraph_confidences.append(paragraph.confidence)
-#                         for word in paragraph.words:
-                            
-#                             for symbol in word.symbols:
-#                                 letterX = [] 
-#                                 letterY = []
-#                                 for coord in symbol.bounding_box.vertices:
-#                                     letterX.append(coord.x) 
-#                                     letterY.append(coord.y)                       
-#                                 Lx1 = letterX[0]
-#                                 Lx2 = letterX[1]
-#                                 Ly1 = letterY[0]
-#                                 Ly3 = letterY[2]
-#                                 width = Lx2 - Lx1
-#                                 height = Ly3 - Ly1  
-#                                 Lsize = {symbol.text: {"width": width, "height": height}}
-                                
-#                                 LsizeCont.append(Lsize)
-#                                 dataJson["Letter-Sizes"] = LsizeCont
-#                 Pconf = math.floor(sum(paragraph_confidences) * 1000)/10/len(paragraph_confidences)
-#                 Bconf = math.floor(sum(block_confidences) * 1000)/10/len(block_confidences)
-#                 dataJson["Paragraph-Confidence"] = Pconf
-#                 dataJson["Block-Confidence"] = Bconf
-#             WsizeCont = []
-#             for listCounter in range(1, len(response.text_annotations)):
-#                 wordBounding = []
-                
-#                 wordData = response.text_annotations[listCounter]
-
-#                 for boundingCoord in wordData.bounding_poly.vertices:
-#                     wordBounding.append(boundingCoord.x)
-#                 Wsize = {wordData.description: wordBounding}
-
-#                 WsizeCont.append(Wsize)
-#                 dataJson["Word-Bounding"] = WsizeCont
-
-#             word_gap_list = []
-#             for i in range(0, len(response.text_annotations)-1):
-#                 if (i > 0):
-#                     W1x = responseObj['textAnnotations'][i]['boundingPoly']['vertices'][1]['x']
-#                     W2x = responseObj['textAnnotations'][i + 1]['boundingPoly']['vertices'][0]['x']
-#                     word_gap = W2x - W1x
-#                     word_gap_list.append(word_gap)
-#             dataJson["Gaps"] = word_gap_list
-#             dataJson["Full-text"] = response.text_annotations[0].description
 #     return dataJson
   
 if __name__ == '__main__':
--- script.js
+++ script.js
@@ -1,49 +1,43 @@
-fetch("http://127.0.0.1:5000/").then(
-    function (response) {
-        return response.json();
-    }
-).then(
-    function (text) {
-        var full_text = text["Full-text"]
-        var edited_text = full_text.replace("\n", "");
-        var list_of_letters = edited_text.replace(/ /g, '');
-        var heightList = [];
-        var widthList = [];
-        var overallConf = Number(((text["Paragraph-Confidence"] + text["Block-Confidence"]) / 2).toFixed(2));
-        for (var x = 0; x < text["Letter-Sizes"].length; x++) {
-            heightList.push(text["Letter-Sizes"][x][list_of_letters[x]]["height"]);
-            widthList.push(text["Letter-Sizes"][x][list_of_letters[x]]["width"]);
-
-            var heightMean = math.mean(heightList);
-            var widthMean = math.mean(widthList);
-            var heightStd = math.std(heightList);
-            var widthStd = math.std(widthList);
-            var heightPercentage = Number((heightStd / heightMean * 100).toFixed(2));
-            var widthPercentage = Number((widthStd / widthMean * 100).toFixed(2));
-            var deviationAvPercentage = Number((100 - ((heightPercentage + widthPercentage) / 2)).toFixed(2));
-            var overallScore = Number(((overallConf + deviationAvPercentage) / 2).toFixed(2));
+function displayPoints() {
+    fetch('http://127.0.0.1:5000/uploaded', {
+        method: 'POST',
+        body: JSON.stringify({
+            url: "https://i.imgur.com/hVXJKTW.png"
+        }),
+        headers: {
+            'Content-Type': "application/json",
+            'Accept': 'application/json'
         }
-
-        var points = math.ceil(math.floor(overallConf) / 5);
-
-
-        document.querySelector("body").innerHTML = "Neatness: " + JSON.stringify(deviationAvPercentage) + "% <br>" + "Handwriting: " + JSON.stringify(overallConf) + "% <br>" + "Overall Score: " + JSON.stringify(overallScore) + "% <br>" + "points scored: " + points;
-
-
     })
+        .then(function (response) {
+            return response.json();
+        }).then(
+            function (text) {
+                var full_text = text["Full-text"];
+                var edited_text = full_text.replace("\n", "");
+                var list_of_letters = edited_text.replace(/ /g, '');
+                var heightList = [];
+                var widthList = [];
+                var overallConf = Number(((text["Paragraph-Confidence"] + text["Block-Confidence"]) / 2).toFixed(2));
+                for (var x = 0; x < text["Letter-Sizes"].length; x++) {
+                    heightList.push(text["Letter-Sizes"][x][list_of_letters[x]]["height"]);
+                    widthList.push(text["Letter-Sizes"][x][list_of_letters[x]]["width"]);
 
-var imgURL = {"url":"https://i.imgur.com/hVXJKTW.png"};
-var x = JSON.stringify(imgURL);
-console.log(x);
+                    var heightMean = math.mean(heightList);
+                    var widthMean = math.mean(widthList);
+                    var heightStd = math.std(heightList);
+                    var widthStd = math.std(widthList);
+                    var heightPercentage = Number((heightStd / heightMean * 100).toFixed(2));
+                    var widthPercentage = Number((widthStd / widthMean * 100).toFixed(2));
+                    var deviationAvPercentage = Number((100 - ((heightPercentage + widthPercentage) / 2)).toFixed(2));
+                    var overallScore = Number(((overallConf + deviationAvPercentage) / 2).toFixed(2));
+                }
 
-// $.ajax({
-//     url:"/uploaded",
-//     type:"POST",
-//     contentType:"application/json",
-//     data: x
-// });
+                var points = math.ceil(math.floor(overallConf) / 5);
 
-fetch('http://127.0.0.1:5000/uploaded', {
-	method: 'POST',
-	body: x
-});
+
+                document.querySelector("body").innerHTML += "<br>Neatness: " + JSON.stringify(deviationAvPercentage) + "% <br>" + "Handwriting: " + JSON.stringify(overallConf) + "% <br>" + "Overall Score: " + JSON.stringify(overallScore) + "% <br>" + "points scored: " + points;
+
+
+            });
+}
