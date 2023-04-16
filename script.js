5085ab5277582345ece23389683d6524f19229e7
manyatapawagi <manyatapawagi@gmail.com>
2023-04-16 18:10:49 +0530

    seventeenth commit

--- script.js
+++ script.js
@@ -8,64 +8,83 @@ const firebaseConfig = {
     measurementId: "G-DHLLLVW8NZ"
 };
 firebase.initializeApp(firebaseConfig);
-
-function displayPoints() {
-    document.querySelector("#send").disabled = true;
-    const ref = firebase.storage().ref();
-    const file = $('#files').get(0).files[0];
-    const name = (+new Date()) + '-' + file.name;
-    const metadata = { contentType: file.type };
-    const task = ref.child(name).put(file, metadata);
-    var imageURL = "";
-    task
-        .then(snapshot => snapshot.ref.getDownloadURL())
-        .then((url) => {
-            imageURL = url;
-            fetch('http://127.0.0.1:5000/uploaded', {
-                method: 'POST',
-                body: JSON.stringify({
-                    url: imageURL
-                }),
-                headers: {
-                    'Content-Type': "application/json",
-                    'Accept': 'application/json'
-                }
-            })
-                .then(function (response) {
-                    return response.json();
+var previous_points;
+if (localStorage.getItem("points") === null) {
+    localStorage.setItem("points", 0);
+    previous_points = localStorage.getItem("points");
+    document.querySelector("#points").innerHTML += previous_points;
+  }
+else {
+    previous_points = localStorage.getItem("points");
+    document.querySelector("#points").innerHTML = "<img src='images/points.png'>" + previous_points;
+}
+  
+function displayPoints() { 
+    if (document.querySelector("#files").value != "") {
+        document.querySelector("#result").style.display = "flex";
+        document.querySelector("#send").disabled = true;
+        
+        const ref = firebase.storage().ref();
+        const file = $('#files').get(0).files[0];
+        const name = (+new Date()) + '-' + file.name;
+        const metadata = { contentType: file.type };
+        const task = ref.child(name).put(file, metadata);
+        var imageURL = "";
+        task
+            .then(snapshot => snapshot.ref.getDownloadURL())
+            .then((url) => {
+                imageURL = url;
+                fetch('http://127.0.0.1:5000/uploaded', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        url: imageURL
+                    }),
+                    headers: {
+                        'Content-Type': "application/json",
+                        'Accept': 'application/json'
+                    }
                 })
-                .then(
-                    function (text) {
-                        var full_text = text["Full-text"];
-                        var edited_text = full_text.replace("\n", "");
-                        var list_of_letters = edited_text.replace(/\s/g,'');
-                        console.log(list_of_letters);
-                        var heightList = [];
-                        var widthList = [];
-                        var overallConf = Number(((text["Paragraph-Confidence"] + text["Block-Confidence"]) / 2).toFixed(2));
-                        for (var x = 0; x < text["Letter-Sizes"].length; x++) {
-                            heightList.push(text["Letter-Sizes"][x][list_of_letters[x]]["height"]);
-                            widthList.push(text["Letter-Sizes"][x][list_of_letters[x]]["width"]);
+                    .then(function (response) {
+                        document.querySelector("#uploading").innerHTML = "Uploaded";
+                        return response.json();
+                    })
+                    .then(
+                        function (text) {
+                            var full_text = text["Full-text"];
+                            var edited_text = full_text.replace("\n", "");
+                            var list_of_letters = edited_text.replace(/\s/g, '');
+                            console.log(list_of_letters);
+                            var heightList = [];
+                            var widthList = [];
+                            var overallConf = Number(((text["Paragraph-Confidence"] + text["Block-Confidence"]) / 2).toFixed(2));
+                            for (var x = 0; x < text["Letter-Sizes"].length; x++) {
+                                heightList.push(text["Letter-Sizes"][x][list_of_letters[x]]["height"]);
+                                widthList.push(text["Letter-Sizes"][x][list_of_letters[x]]["width"]);
 
-                            var heightMean = math.mean(heightList);
-                            var widthMean = math.mean(widthList);
-                            var heightStd = math.std(heightList);
-                            var widthStd = math.std(widthList);
-                            var heightPercentage = Number((heightStd / heightMean * 100).toFixed(2));
-                            var widthPercentage = Number((widthStd / widthMean * 100).toFixed(2));
-                            var deviationAvPercentage = Number((100 - ((heightPercentage + widthPercentage) / 2)).toFixed(2));
-                            var overallScore = Number(((overallConf + deviationAvPercentage) / 2).toFixed(2));
-                        }
+                                var heightMean = math.mean(heightList);
+                                var widthMean = math.mean(widthList);
+                                var heightStd = math.std(heightList);
+                                var widthStd = math.std(widthList);
+                                var heightPercentage = Number((heightStd / heightMean * 100).toFixed(2));
+                                var widthPercentage = Number((widthStd / widthMean * 100).toFixed(2));
+                                var deviationAvPercentage = Number((100 - ((heightPercentage + widthPercentage) / 2)).toFixed(2));
+                                var overallScore = Number(((overallConf + deviationAvPercentage) / 2).toFixed(2));
+                            }
 
-                        var points = math.ceil(math.floor(overallConf) / 5);
+                            var points = math.ceil(math.floor(overallConf) / 5);
+                            var new_points = Number(points) + Number(previous_points);
+                            localStorage.setItem("points", new_points);
+                            document.querySelector("#result").innerHTML += "<br>Neatness: " + JSON.stringify(deviationAvPercentage) + "% <br>" + "Handwriting: " + JSON.stringify(overallConf) + "% <br>" + "Overall Score: " + JSON.stringify(overallScore) + "% <br>" + "points scored: " + points + "<br><button class='button' id='send' onclick='displayPoints()'><p>HOME</p></button>";
+                            document.querySelector("#points").innerHTML = "<img src='images/points.png'>" + localStorage.getItem("points")
+                            plotGraph(text);
 
-                        document.querySelector("#result").innerHTML += "<br>Neatness: " + JSON.stringify(deviationAvPercentage) + "% <br>" + "Handwriting: " + JSON.stringify(overallConf) + "% <br>" + "Overall Score: " + JSON.stringify(overallScore) + "% <br>" + "points scored: " + points;
+                        });
 
-                        plotGraph(text);
-
-                    });
-
-        }).catch(console.error);
+            }).catch(console.error);
+    }
+    else {
+        alert("Please upload an image first")
+    }
 }
 
 function plotGraph(data) {
@@ -79,25 +98,25 @@ function plotGraph(data) {
         labels.push(char);
     });
     var averageHeight = 0;
-    heightsData.forEach(function(h){averageHeight += h;});
+    heightsData.forEach(function (h) { averageHeight += h; });
     averageHeight /= heightsData.length;
-    deviationData = heightsData.map(function(h){
+    deviationData = heightsData.map(function (h) {
         return h - averageHeight;
     })
     new Chart("myChart", {
         type: "line",
         data: {
-          labels: labels,
-          datasets: [{
-            fill: false,
-            lineTension: 0,
-            backgroundColor: "rgba(0,0,255,1.0)",
-            borderColor: "rgba(0,0,255,0.1)",
-            data: deviationData
-          }]
+            labels: labels,
+            datasets: [{
+                fill: false,
+                lineTension: 0,
+                backgroundColor: "rgba(0,0,255,1.0)",
+                borderColor: "rgba(0,0,255,0.1)",
+                data: deviationData
+            }]
         },
         options: {
-          legend: {display: false}
+            legend: { display: false }
         }
     });
 }
